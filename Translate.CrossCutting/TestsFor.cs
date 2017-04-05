840bfbea95e67229b3d6dcecb33865967fc6f28b
Pedro Dias <pedro@digitaldias.com>
2017-04-05 12:18:37 +0200

    Add Cross-Cutting Layer with baseclass for all Unit Tests
    The TestsFor<TEntity> class takes a dependency on structuremap.automocking.moq

---
+++ Translate.CrossCutting/TestsFor.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using StructureMap.AutoMocking.Moq;
+
+namespace Translate.CrossCutting
+{
+    public class TestsFor<TEntity> where TEntity : class
+    {
+        protected TEntity Instance { get; set; }
+        protected MoqAutoMocker<TEntity> AutoMocker { get; set; }
+
+        public TestsFor()
+        {
+            AutoMocker = new MoqAutoMocker<TEntity>();
+
+            Instance = AutoMocker.ClassUnderTest;
+        }
+
+
+        public Mock<TContract> GetMockFor<TContract>() where TContract : class
+        {
+            return Mock.Get(AutoMocker.Get<TContract>());
+        }
+    }
+}
